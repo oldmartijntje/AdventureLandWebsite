dc94d8abccec7109f23ba87d2ef2aa2442a0693a
Martijn <oldmartijntje@gmail.com>
2023-10-23 12:25:06 +0200

    lolcat

---
+++ meow/data.js
@@ -0,0 +1,84 @@
+const dataVersion = "v0.1.0";
+const imageUrls = [
+    'assets/Adventureland_3D.jpg',
+    'assets/Cover 2D.jpg',
+    'assets/The Gorilla Shake.jpg',
+    'assets/Snack Sparrow.jpg',
+    'assets/Trucks Playing Tricks.jpg',
+    'assets/Toilet Ladies.jpg',
+    'assets/Tronville.jpg',
+    'assets/Rock & Roll Ride.jpg',
+    'assets/Perfect waves.jpg',
+    'assets/Miami Vibes.jpg',
+    'assets/Daddy Love.jpg',
+    'assets/Award card.jpg',
+
+];
+
+const reviews = [
+    {
+        name: "markmasters",
+        rating: 9,
+        comment: "Played at Spiel. Fun game with a nice mechanic that allows either to pick first or grants money.",
+        link: "https://boardgamegeek.com/boardgame/356630/adventure-land/ratings#:~:text=9-,markmasters,-Oct%202023",
+    },
+    {
+        name: "oldmartijntje",
+        rating: 10,
+        comment: "Awesome game, fun for all ages. The art is on point, with a lot of fun jokes. The production quality is good. The gameplay mechanics are very fun. Everyone I talk to about this game likes it. Even if you think you are losing mid game, you can still win in the end. I would recommend this to pretty much every type of player. Because you can play it casually or very strategic, and both types of players can win.",
+        link: "https://boardgamegeek.com/boardgame/356630/adventure-land/ratings#:~:text=10-,oldmartijntje,-Oct%202023",
+    },
+    {
+        name: "hollandiase",
+        comment: "Wat een leuk spel is dit! Bieden, bluffen, betalen,sparen,setcollection.. er zit van alles in. En daarnaast ook nog eens spannend!",
+        link: "https://www.instagram.com/p/CyBzqv1Lm8m/?igshid=MzRlODBiNWFlZA==",
+    },
+    {
+        name: "Brainmaster",
+        rating: 9,
+        comment: "",
+        link: "https://boardgamegeek.com/boardgame/356630/adventure-land/ratings#:~:text=9-,Brainmaster,-Sep%202023",
+    },
+    {
+        name: "Mielesgames",
+        rating: 10,
+        comment: "It's an awesome game, 10/10 it's perfect!",
+        link: "https://boardgamegeek.com/boardgame/356630/adventure-land/ratings#:~:text=10-,Mielesgames,-Oct%202023",
+    },
+    {
+        name: "de Beek",
+        rating: 8,
+        comment: "",
+        link: "https://boardgamegeek.com/boardgame/356630/adventure-land/ratings#:~:text=8-,de%20Beek,-Oct%202023",
+    },
+    {
+        name: "bridgetvh",
+        rating: 8,
+        comment: "",
+        link: "https://boardgamegeek.com/thread/3159354/review-bordspelwereldnl-dutch",
+    },
+];
+
+const translations = {
+    "cat": {
+        "T.title": "Adventure Land Boawd Game",
+        "T.headerTitle": "Adventure Land - Buildz Yer Dreamz Park",
+        "prevButton": "Prevy",
+        "nextButton": "Nexty",
+        "T.infoTitle1": "Buildz Yer Dreamz Park!",
+        "T.infoText1": "In Adventure Land, yer journey startz to creatin' da most excitin' themepark. Buildz thrillin' attraqtions, hirez da bestest staff, an' investz yer moolah wisely. Complete seekrit mishuns to boostz yer park's popoolarity. Da ultimate goal be to wins da prestiged Award!",
+        "T.infoTitle2": "A Majestic Adventurz",
+        "T.infoText2": "Adventure Land feelz like a rollercoaster of epic proportions. So much happinz in just an hour dat ya needz a bit of recup'ration. It all kickz off with park auctions. Every park lookz entirely diff'rent in each game. Which one ya chooz? This head start offerz specific chanzes an' possible strategiez from da outset. Will ya specialize in a particular theme? Or will ya hire valyooable staff? Decide to employ janitors an' earn scratch with every built toilet, or play it safe an' take on a gardener for a stable income stream? Da choiz be yers!",
+        "T.reviewsTitle": "Reviewz",
+        "prevReviewButton": "Prevy Review",
+        "nextReviewButton": "Nexty Review",
+        "T.linksTitle": "Linkz:",
+        "T.instagram": "InstaMeow",
+        "T.theGameMaster": "DaGameMasta",
+        "T.explanationVideo": "Explenashun Meowtube Video",
+        "T.boardGameGeek": "BoardGameKitteh",
+        "download-link": "Download Rulez PDF",
+        "T.reviewLink": "Orig'nul Post",
+        "T.reviewRating": "Rating"
+    }
+};
